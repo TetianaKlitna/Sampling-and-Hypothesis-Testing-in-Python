f604e32feb098898905e19b8f79494376c59d326
TetianaKlitna <146033253+TetianaKlitna@users.noreply.github.com>
2024-02-25 13:44:20 -0800

    Add files via upload

---
+++ Proportional_stratified_sampling.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Proportion of employees by Education level
+education_counts_pop = attrition_pop['Education'].value_counts(normalize=True)
+
+# Print education_counts_pop
+print(education_counts_pop)
+
+# Proportional stratified sampling for 40% of each Education group
+attrition_strat = attrition_pop.groupby('Education')\
+	.sample(frac=0.4, random_state=2022)
+
+# Calculate the Education level proportions from attrition_strat
+education_counts_strat = attrition_strat['Education'].value_counts(normalize=True)
+
+# Print education_counts_strat
+print(education_counts_strat)
+
+# Get 30 employees from each Education group
+attrition_eq = attrition_pop.groupby('Education')\
+	.sample(n=30, random_state=2022)      
+
+# Get the proportions from attrition_eq
+education_counts_eq = attrition_eq['Education'].value_counts(normalize = True)
+
+# Print the results
+print(education_counts_eq)
